db48c1797788c3f9854df73d56775b8a306b783d
Todd0042 <scottrb1982@gmail.com>
2026-04-23 09:25:24 -0400

    added nexus metadata

--- src/addon.cpp
+++ src/addon.cpp
@@ -34,13 +34,13 @@ AddonDefinition_t* GetAddonDef() {
     s_addonDef.APIVersion   = NEXUS_API_VERSION;
     s_addonDef.Name         = "Waitingame";
     s_addonDef.Version      = { 0, 1, 0, 0 };
-    s_addonDef.Author       = "todd";
+    s_addonDef.Author       = "Todd0042";
     s_addonDef.Description  = "Shmup to pass the time waiting for GW2 events";
     s_addonDef.Load         = AddonLoad;
     s_addonDef.Unload       = AddonUnload;
     s_addonDef.Flags        = AF_None;
-    s_addonDef.Provider     = UP_None;
-    s_addonDef.UpdateLink   = nullptr;
+    s_addonDef.Provider     = UP_GitHub;
+    s_addonDef.UpdateLink   = "https://github.com/Todd0042/gw2-Waitingame";
     return &s_addonDef;
 }
 
